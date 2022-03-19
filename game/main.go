6436a07d79308abfbebf9e0e8fd72756d3546323
kraxarn <kraxie@protonmail.com>
2022-03-19 13:44:30 +0100

    Added logo

--- game/main.go
+++ game/main.go
@@ -2,6 +2,7 @@ package game
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/kraxarn/ubongo/assets"
 	"github.com/kraxarn/ubongo/color"
 	"github.com/kraxarn/ubongo/enum"
 	"github.com/kraxarn/ubongo/util"
@@ -12,6 +13,7 @@ import (
 type Game struct {
 	ui   *widget.Ui
 	size util.Vector2[int]
+	logo *widget.Image
 }
 
 func NewGame() *Game {
@@ -20,15 +22,29 @@ func NewGame() *Game {
 		log.Fatal(err)
 	}
 
+	imgLogo, err := assets.Image("logo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Logo
+	logo := ui.AddImage(imgLogo, 0, 0, 64, 64)
+
+	// Buttons
 	ui.AddStretchedButton(widget.ScreenPadding*4+widget.ButtonHeight, enum.AlignBottom, "Start Game")
 	ui.AddStretchedButton(widget.ScreenPadding*3, enum.AlignBottom, "Settings")
 
 	return &Game{
-		ui: ui,
+		ui:   ui,
+		logo: logo,
 	}
 }
 
 func (g *Game) Update() error {
+	// Update logo size and position
+	g.logo.SetWidth(g.size.X / 2)
+	g.logo.SetPosition(g.size.X/2-(g.logo.GetWidth()/2), int(float64(g.size.Y)*0.1))
+
 	g.ui.Update(g.size)
 	return nil
 }
--- widget/image.go
+++ widget/image.go
@@ -2,24 +2,24 @@ package widget
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
-	"image"
+	"github.com/kraxarn/ubongo/util"
 )
 
 type Image struct {
-	image  *ebiten.Image
-	rect   image.Rectangle
-	scaleX float64
-	scaleY float64
+	image    *ebiten.Image
+	scaleX   float64
+	scaleY   float64
+	position util.Vector2[int]
 }
 
 func NewImage(src *ebiten.Image, x, y, w, h int) *Image {
-	imgWidth, imgHeight := src.Size()
-	return &Image{
-		image:  src,
-		rect:   image.Rect(x, y, x+w, y+h),
-		scaleX: float64(w) / float64(imgWidth),
-		scaleY: float64(h) / float64(imgHeight),
+	img := &Image{
+		image: src,
 	}
+
+	img.SetPosition(x, y)
+	img.Resize(w, h)
+	return img
 }
 
 func (i *Image) Update(_ *Ui) {
@@ -29,5 +29,28 @@ func (i *Image) Update(_ *Ui) {
 func (i *Image) Draw(dst *ebiten.Image) {
 	opt := &ebiten.DrawImageOptions{}
 	opt.GeoM.Scale(i.scaleX, i.scaleY)
+	opt.GeoM.Translate(float64(i.position.X), float64(i.position.Y))
+
 	dst.DrawImage(i.image, opt)
+}
+
+func (i *Image) Resize(w, h int) {
+	imgWidth, imgHeight := i.image.Size()
+	i.scaleX = float64(w) / float64(imgWidth)
+	i.scaleY = float64(h) / float64(imgHeight)
+}
+
+func (i *Image) GetWidth() int {
+	w, _ := i.image.Size()
+	return int(float64(w) * i.scaleX)
+}
+
+func (i *Image) SetWidth(w int) {
+	imgWidth, imgHeight := i.image.Size()
+	i.scaleX = float64(w) / float64(imgWidth)
+	i.scaleY = float64(w) / float64(imgHeight)
+}
+
+func (i *Image) SetPosition(x, y int) {
+	i.position = util.Vec2(x, y)
 }
