b2ad653ad3994704da4dbe6832960dc0d00a82c6
kraxarn <kraxie@protonmail.com>
2022-03-20 22:43:12 +0100

    Fullscreen on Android

--- main.go
+++ main.go
@@ -4,12 +4,19 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/kraxarn/ubongo/game"
 	"log"
+	"runtime"
 )
 
 func main() {
-	ebiten.SetWindowSize(540, 960)
 	ebiten.SetWindowTitle("Ubongo")
 	ebiten.SetWindowResizable(true)
+
+	// Fullscreen on mobile, 540x960 on desktop
+	if runtime.GOOS == "android" {
+		ebiten.SetFullscreen(true)
+	} else {
+		ebiten.SetWindowSize(540, 960)
+	}
 
 	err := ebiten.RunGame(game.NewGame())
 	if err != nil {
