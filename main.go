f819003ffda21e1c542dcc74c8e596cca65aec3b
kraxarn <kraxie@protonmail.com>
2022-03-25 14:50:34 +0100

    Move app name to constant in package

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/kraxarn/ubongo/app"
 	"github.com/kraxarn/ubongo/game"
 	"log"
 )
 
 func main() {
 	ebiten.SetWindowSize(540, 960)
-	ebiten.SetWindowTitle("Ubongo")
+	ebiten.SetWindowTitle(app.Name)
 	ebiten.SetWindowResizable(true)
 
 	err := ebiten.RunGame(game.NewGame())
