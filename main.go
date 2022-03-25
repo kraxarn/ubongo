6837ed2204c9c98ab67bc23f402fcfa3d903d551
kraxarn <kraxie@protonmail.com>
2022-03-25 13:25:43 +0100

    SetWindowSize doesn't do anything on Android

--- main.go
+++ main.go
@@ -4,19 +4,12 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/kraxarn/ubongo/game"
 	"log"
-	"runtime"
 )
 
 func main() {
+	ebiten.SetWindowSize(540, 960)
 	ebiten.SetWindowTitle("Ubongo")
 	ebiten.SetWindowResizable(true)
-
-	// Fullscreen on mobile, 540x960 on desktop
-	if runtime.GOOS == "android" {
-		ebiten.SetFullscreen(true)
-	} else {
-		ebiten.SetWindowSize(540, 960)
-	}
 
 	err := ebiten.RunGame(game.NewGame())
 	if err != nil {
