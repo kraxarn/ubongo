be656c73a5f381b294441d0dd1663186ae5b8a82
kraxarn <kraxie@protonmail.com>
2022-04-14 17:25:53 +0200

    Store as float64

--- game/settings/settings.go
+++ game/settings/settings.go
@@ -5,23 +5,35 @@ import (
 )
 
 type Settings struct {
-	MusicVolume float32
+	MusicVolume float64
 }
 
 func Load() Settings {
 	return Settings{
-		MusicVolume: getFloat32("musicVolume", 0),
+		MusicVolume: getFloat64("musicVolume", 0),
 	}
 }
 
 func (s *Settings) Save() {
-	set("musicVolume", strconv.FormatFloat(float64(s.MusicVolume), 'f', 2, 32))
+	set("musicVolume", strconv.FormatFloat(s.MusicVolume, 'f', 2, 64))
 }
 
-func getFloat32(key string, fallback float32) float32 {
-	value, err := strconv.ParseFloat(get(key), 32)
+func getFloat64(key string, fallback float64) float64 {
+	value, err := strconv.ParseFloat(get(key), 64)
 	if err == nil {
-		return float32(value)
+		return value
 	}
 	return fallback
+}
+
+func (s *Settings) ToggleMusic(enabled bool) {
+	if enabled {
+		s.MusicVolume = 0.50
+	} else {
+		s.MusicVolume = 0
+	}
+}
+
+func (s *Settings) IsMusicEnabled() bool {
+	return s.MusicVolume > 0
 }
