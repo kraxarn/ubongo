f6dbfd22a0c90fe657ec7b852b50ed7176ea9199
kraxarn <kraxie@protonmail.com>
2022-04-14 15:15:30 +0200

    Fix calling wrong Update/Draw

--- widget/stretched_button.go
+++ widget/stretched_button.go
@@ -26,16 +26,16 @@ func NewStretchedButton(background *ebiten.Image, fontFace font.Face, x, y, w, h
 func (s *StretchedButton) Update(ui *Ui) {
 	// Because the button stretches, alignment on x doesn't matter
 	if s.x >= 0 {
-		s.Button.rect.Max.X = ui.screenSize.X - ScreenPadding
+		s.TextButton.rect.Max.X = ui.screenSize.X - ScreenPadding
 	}
 
 	if s.y >= 0 {
-		ui.setAbsoluteY(&s.Button.rect, s.y, s.align)
+		ui.setAbsoluteY(&s.TextButton.rect, s.y, s.align)
 	}
 
-	s.Button.Update(ui)
+	s.TextButton.Update(ui)
 }
 
 func (s *StretchedButton) Draw(dst *ebiten.Image) {
-	s.Button.Draw(dst)
+	s.TextButton.Draw(dst)
 }
