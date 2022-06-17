837aa60ad8e5d8c3a65cef60357d856c8b1cbe4d
kraxarn <kraxie@protonmail.com>
2022-06-17 20:54:26 +0200

    Initial text sizes

---
+++ src/commonMain/kotlin/constants/TextSize.kt
@@ -0,0 +1,19 @@
+package constants
+
+object TextSize
+{
+	/**
+	 * Default font size for buttons
+	 */
+	const val button = 42.0
+
+	/**
+	 * Default font size for titles
+	 */
+	const val title = 96.0
+
+	/**
+	 * Default font size for smaller (debug) text
+	 */
+	const val debug = 18.0
+}
