e13327bab3e977192e28dee85fe6c7a1b922dd1a
kraxarn <kraxie@protonmail.com>
2022-06-18 10:35:26 +0200

    More final button skin

--- src/commonMain/kotlin/skins/ButtonSkin.kt
+++ src/commonMain/kotlin/skins/ButtonSkin.kt
@@ -1,28 +1,34 @@
 package skins
 
 import com.soywiz.korge.ui.*
-import com.soywiz.korim.bitmap.Bitmap
 import com.soywiz.korim.bitmap.NativeImage
-import com.soywiz.korim.bitmap.asNinePatch
+import com.soywiz.korim.bitmap.NinePatchBmpSlice
+import com.soywiz.korim.bitmap.asNinePatchSimple
 import com.soywiz.korim.bitmap.context2d
 import com.soywiz.korim.color.Colors
+import com.soywiz.korim.color.RGBA
+import com.soywiz.korim.format.writeTo
+import com.soywiz.korio.async.runBlockingNoJs
+import com.soywiz.korio.file.std.applicationVfs
 import com.soywiz.korio.lang.InvalidArgumentException
-import com.soywiz.korma.geom.vector.roundRect
+import extensions.roundRect
 
 class ButtonSkin : UISkin()
 {
-
 	init
 	{
-		buttonNormal = buildButton(UiSkinType.NORMAL).asNinePatch()
-		buttonOver = buildButton(UiSkinType.OVER).asNinePatch()
-		buttonDown = buildButton(UiSkinType.DOWN).asNinePatch()
-		buttonDisabled = buildButton(UiSkinType.DISABLED).asNinePatch()
+		buttonNormal = buildButton(UiSkinType.NORMAL)
+		buttonOver = buildButton(UiSkinType.OVER)
+		buttonDown = buildButton(UiSkinType.DOWN)
+		buttonDisabled = buildButton(UiSkinType.DISABLED)
 	}
 
-	private fun buildButton(type: UiSkinType): Bitmap
+	private fun buildButton(type: UiSkinType): NinePatchBmpSlice
 	{
-		return NativeImage(BUTTON_SIZE, BUTTON_SIZE).context2d {
+		val shadow = type == UiSkinType.NORMAL
+			|| type == UiSkinType.OVER
+
+		val img = NativeImage(BUTTON_SIZE, BUTTON_SIZE).context2d {
 			val fillColor = when (type)
 			{
 				UiSkinType.NORMAL -> Colors["#3f51b5"]   // Indigo 500
@@ -32,20 +38,58 @@ class ButtonSkin : UISkin()
 				else -> throw InvalidArgumentException(type.toString())
 			}
 
+			if (shadow)
+			{
+				val shadowAlpha = (when (type)
+				{
+					UiSkinType.OVER -> 0.3
+					UiSkinType.DISABLED -> 0.7
+					else -> 0.5
+				} * 255).toInt()
+
+				fill(RGBA(0, 0, 0, shadowAlpha)) {
+					roundRect(
+						PADDING + SHADOW_WIDTH,
+						BUTTON_SIZE - PADDING * 2 - SHADOW_WIDTH,
+						BORDER_RADIUS
+					)
+				}
+			}
+
 			fill(fillColor) {
 				roundRect(
-					PADDING, PADDING,
-					BUTTON_SIZE - PADDING * 2, BUTTON_SIZE - PADDING * 2,
-					BORDER_RADIUS, BORDER_RADIUS
+					PADDING
+						+ if (type == UiSkinType.DOWN) SHADOW_WIDTH else 0,
+					BUTTON_SIZE - PADDING * 2
+						- (if (shadow || type == UiSkinType.DISABLED || type == UiSkinType.DOWN) SHADOW_WIDTH else 0),
+					BORDER_RADIUS
 				)
 			}
 		}
+
+		runBlockingNoJs {
+			img.writeTo(applicationVfs["button/${type.name}.png"])
+		}
+
+		return img.asNinePatchSimple(INSET, INSET, INSET, INSET)
 	}
 
 	companion object
 	{
 		const val BUTTON_SIZE = 64
 		const val PADDING = 4
-		const val BORDER_RADIUS = 6
+		const val BORDER_RADIUS = 12
+		const val SHADOW_WIDTH = 10
+		const val INSET = 22
 	}
 }
+
+private val UiSkinType.name
+	get() = when (this)
+	{
+		UiSkinType.NORMAL -> "normal"
+		UiSkinType.OVER -> "over"
+		UiSkinType.DOWN -> "down"
+		UiSkinType.DISABLED -> "disabled"
+		else -> null
+	}
