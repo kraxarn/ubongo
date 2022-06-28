096b0393ccfb7c86479d2e809a979cebce64a570
kraxarn <kraxie@protonmail.com>
2022-06-28 21:28:11 +0200

    maxWidthOrHeight extension

--- src/commonMain/kotlin/extensions/ViewExtensions.kt
+++ src/commonMain/kotlin/extensions/ViewExtensions.kt
@@ -1,6 +1,11 @@
 package extensions
 
 import com.soywiz.korge.view.View
+import com.soywiz.korge.view.scale
 import com.soywiz.korma.geom.Point
+import kotlin.math.max
 
 val View.size2 get() = Point(this.width, this.height)
+
+fun <T : View> T.maxWidthOrHeight(scale: Double) =
+	this.scale(scale / max(this.width, this.height))
