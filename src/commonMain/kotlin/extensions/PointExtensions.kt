dc76f43ed4dde05766e27eb851db341ba68e606d
kraxarn <kraxie@protonmail.com>
2022-06-28 10:27:32 +0200

    Remove unused extension

--- src/commonMain/kotlin/extensions/PointExtensions.kt
+++ src/commonMain/kotlin/extensions/PointExtensions.kt
@@ -1,12 +1,8 @@
 package extensions
 
 import com.soywiz.korma.geom.IPoint
-import com.soywiz.korma.geom.Point
 import com.soywiz.korma.geom.PointInt
 import kotlin.math.round
-
-operator fun IPoint.rem(value: Double) =
-	Point(this.x % value, this.y % value)
 
 fun IPoint.toInt() =
 	PointInt(round(this.x).toInt(), round(this.y).toInt())
