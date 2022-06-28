0821af4bad2e23a709065b0287fd22f15bae44c0
kraxarn <kraxie@protonmail.com>
2022-06-28 21:31:35 +0200

    Initial pausing

--- src/commonMain/kotlin/scenes/GameScene.kt
+++ src/commonMain/kotlin/scenes/GameScene.kt
@@ -4,21 +4,26 @@ import GameState
 import com.soywiz.klock.TimeSpan
 import com.soywiz.korge.annotations.KorgeExperimental
 import com.soywiz.korge.input.draggable
+import com.soywiz.korge.input.onClick
+import com.soywiz.korge.input.onMouseDrag
 import com.soywiz.korge.scene.Scene
 import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.*
+import com.soywiz.korim.bitmap.Bitmap
+import com.soywiz.korim.font.Font
 import com.soywiz.korim.font.readTtfFont
+import com.soywiz.korim.vector.format.SVG
+import com.soywiz.korim.vector.render
 import com.soywiz.korio.file.std.resourcesVfs
 import com.soywiz.korio.util.toStringDecimal
 import com.soywiz.korma.geom.Point
 import constants.GameColors
 import constants.TextSize
 import containers.Board
+import containers.PauseDialog
 import containers.Piece
-import extensions.nextPoint
-import extensions.pieceShapes
-import extensions.size2
-import extensions.toInt
+import containers.pauseDialog
+import extensions.*
 
 @KorgeExperimental
 class GameScene(private val gameState: GameState) : Scene()
@@ -26,13 +31,31 @@ class GameScene(private val gameState: GameState) : Scene()
 	private lateinit var titleSkin: UISkin
 	private lateinit var textSkin: UISkin
 
+	private lateinit var pauseIcon: Bitmap
+	private lateinit var pauseButton: Image
+
+	private var pauseDialog: PauseDialog? = null
+	private var dialogBackdrop: View? = null
+
+	private var paused
+		get() = pauseDialog?.visible == true || dialogBackdrop?.visible == true
+		set(value)
+		{
+			pauseDialog?.visible = value
+			dialogBackdrop?.visible = value
+		}
+
+	lateinit var boldFont: Font
+	lateinit var regularFont: Font
+
 	private var duration = TimeSpan.NIL
 
 	private lateinit var pieces: List<Piece>
 
 	override suspend fun Container.sceneInit()
 	{
-		val regularFont = resourcesVfs["fonts/regular.ttf"].readTtfFont()
+		boldFont = resourcesVfs["fonts/bold.ttf"].readTtfFont()
+		regularFont = resourcesVfs["fonts/regular.ttf"].readTtfFont()
 		val lightFont = resourcesVfs["fonts/light.ttf"].readTtfFont()
 
 		titleSkin = UISkin {
@@ -46,6 +69,8 @@ class GameScene(private val gameState: GameState) : Scene()
 			textColor = GameColors.foregroundAlt
 			textSize = TextSize.button
 		}
+
+		pauseIcon = SVG(resourcesVfs["images/ui/pause.svg"].readString()).render()
 	}
 
 	override suspend fun Container.sceneMain()
@@ -68,6 +93,7 @@ class GameScene(private val gameState: GameState) : Scene()
 			}
 
 			addUpdater {
+				if (paused || duration == TimeSpan.NIL) return@addUpdater
 				duration += it
 				time.text = duration.seconds.toStringDecimal(1)
 			}
@@ -80,6 +106,13 @@ class GameScene(private val gameState: GameState) : Scene()
 		val board = Board(gameState.random, pieceShapes, size).addTo(this) {
 			position(PADDING, 0.0)
 			alignTopToBottomOf(hud, PADDING)
+		}
+
+		pauseButton = image(pauseIcon) {
+			maxWidthOrHeight(96.0)
+			alignRightToRightOf(board, PADDING / 2)
+			alignBottomToTopOf(board, PADDING / 4)
+			onClick { paused = true }
 		}
 
 		pieces = pieceShapes
@@ -114,6 +147,20 @@ class GameScene(private val gameState: GameState) : Scene()
 				}
 				else piece.position(it.viewNextXY)
 			}
+		}
+
+		dialogBackdrop = solidRect(views.actualVirtualWidth, views.actualVirtualHeight, GameColors.dialogBackdrop) {
+			position(views.actualVirtualLeft, views.actualVirtualTop)
+			visible = false
+			// Prevent pieces from being moved
+			onMouseDrag { }
+		}
+
+		pauseDialog = pauseDialog(views.virtualWidth * 0.75, views.virtualHeight * 0.25) {
+			position(views.virtualWidth * 0.125, views.virtualHeight / 2 - height / 2)
+			visible = false
+			onBack { sceneContainer.back() }
+			onResume { paused = false }
 		}
 	}
 
