ba5d06ea1dd046afae71ca704fd64a1cd0140aa2
kraxarn <kraxie@protonmail.com>
2022-07-04 20:34:56 +0200

    New bold italic font

--- src/commonMain/kotlin/enums/ResFont.kt
+++ src/commonMain/kotlin/enums/ResFont.kt
@@ -3,6 +3,7 @@ package enums
 enum class ResFont(val path: String)
 {
 	BOLD("fonts/bold.ttf"),
+	BOLD_ITALIC("fonts/bold-italic.ttf"),
 	LIGHT("fonts/light.ttf"),
 	REGULAR("fonts/regular.ttf"),
 }
