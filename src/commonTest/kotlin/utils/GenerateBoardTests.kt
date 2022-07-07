802c0d54cf1b66eca1e9caa0155b9a1787f0ed07
kraxarn <kraxie@protonmail.com>
2022-07-07 11:29:50 +0200

    Test for generating 100 boards without any errors

---
+++ src/commonTest/kotlin/utils/GenerateBoardTests.kt
@@ -0,0 +1,39 @@
+package utils
+
+import com.soywiz.kds.random.FastRandom
+import com.soywiz.klogger.Logger
+import enums.Difficulty
+import enums.PieceShape
+import helpers.MemoryLogOutput
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GenerateBoardTests
+{
+	@Test
+	fun canGenerateBoardWithoutWarnings()
+	{
+		for (i in 0L..100L)
+		{
+			for (difficulty in Difficulty.values())
+			{
+				val warnings = getWarningsOnGeneration(i, difficulty)
+				assertEquals(0, warnings, "$difficulty ($i)")
+			}
+		}
+	}
+
+	private fun getWarningsOnGeneration(seed: Long, difficulty: Difficulty): Int
+	{
+		val log = Logger("generateBoard")
+		val logOutput = MemoryLogOutput()
+		log.output = logOutput
+
+		generateBoard(
+			FastRandom(seed), PieceShape.values().toList(),
+			difficulty.pieceCount, difficulty.rotation
+		)
+
+		return logOutput[Logger.Level.WARN]?.count() ?: 0
+	}
+}
