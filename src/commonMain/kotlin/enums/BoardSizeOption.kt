86dc1487ff04a58fe12f243cbe8ec1c50678492b
kraxarn <kraxie@protonmail.com>
2022-07-07 10:40:17 +0200

    Remove old option enums

--- src/commonMain/kotlin/enums/BoardSizeOption.kt
+++
@@ -1,8 +0,0 @@
-package enums
-
-enum class BoardSizeOption(val displayName: String, val value: Int)
-{
-	SMALL("5x5", 5),
-	MEDIUM("8x8", 8),
-	LARGE("12x12", 12),
-}
--- src/commonMain/kotlin/enums/RotationOption.kt
+++
@@ -1,7 +0,0 @@
-package enums
-
-enum class RotationOption(val displayName: String, val value: Boolean)
-{
-	ON("On", true),
-	OFF("Off", false),
-}
