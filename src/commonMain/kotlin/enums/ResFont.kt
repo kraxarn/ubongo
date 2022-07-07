738fb27d515d713178cd3a4d2dcc1f932bf93ca4
kraxarn <kraxie@protonmail.com>
2022-07-07 11:51:25 +0200

    Remove unused font

--- src/commonMain/kotlin/enums/ResFont.kt
+++ src/commonMain/kotlin/enums/ResFont.kt
@@ -3,7 +3,6 @@ package enums
 enum class ResFont(val path: String)
 {
 	BOLD("fonts/bold.ttf"),
-	BOLD_ITALIC("fonts/bold-italic.ttf"),
 	LIGHT("fonts/light.ttf"),
 	REGULAR("fonts/regular.ttf"),
 }
