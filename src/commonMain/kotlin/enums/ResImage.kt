5b3ea8c7c9976f6712c852ece2426e99fafe9b5d
kraxarn <kraxie@protonmail.com>
2022-07-10 12:34:14 +0200

    Remove unused images

--- src/commonMain/kotlin/enums/ResImage.kt
+++ src/commonMain/kotlin/enums/ResImage.kt
@@ -5,6 +5,4 @@ enum class ResImage(val path: String)
 	UI_ARROW_LEFT("images/ui/arrow-left.svg"),
 	UI_PAUSE("images/ui/pause.svg"),
 	UI_PLAY("images/ui/play.svg"),
-	UI_VOLUME_HIGH("images/ui/volume-high.svg"),
-	UI_VOLUME_XMARK("images/ui/volume-xmark.svg"),
 }
