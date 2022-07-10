ecd18bc0f155c942832bafc358354e3dd26c41f6
kraxarn <kraxie@protonmail.com>
2022-07-10 16:10:58 +0200

    New icons for settings

--- src/commonMain/kotlin/enums/ResImage.kt
+++ src/commonMain/kotlin/enums/ResImage.kt
@@ -3,6 +3,9 @@ package enums
 enum class ResImage(val path: String)
 {
 	UI_ARROW_LEFT("images/ui/arrow-left.svg"),
+	UI_GEAR("images/ui/gear.svg"),
 	UI_PAUSE("images/ui/pause.svg"),
 	UI_PLAY("images/ui/play.svg"),
+	UI_SQUARE("images/ui/square.svg"),
+	UI_SQUARE_CHECK("images/ui/square-check.svg"),
 }
